ea2380643ad0cb7e24849686c29edd3568ac7d4e
MomoRei <wlee@hmc.edu>
2015-07-12 01:12:14 -0700

    removed template

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,17 +1,17 @@
 (function() {
 	var app = angular.module('app',['ngRoute', 'ngResource', 'mapApp','ui.bootstrap', 'mapCtrl','mapService','ngMaterial']);
 
-	app.config(['$routeProvider', '$locationProvider',
-		function($routeProvider, $locationProvider){
-			$routeProvider
-			.when('/locations',
-			{
-				templateUrl:  "assets/home.html",
-			})
-			.when('/',
-			{
-				templateUrl:  "assets/home.html",
-			});
-			$locationProvider.html5Mode(true);
-		}]);
+	// app.config(['$routeProvider', '$locationProvider',
+	// 	function($routeProvider, $locationProvider){
+	// 		$routeProvider
+	// 		.when('/locations',
+	// 		{
+	// 			templateUrl:  "assets/home.html",
+	// 		})
+	// 		.when('/',
+	// 		{
+	// 			templateUrl:  "assets/home.html",
+	// 		});
+	// 		$locationProvider.html5Mode(true);
+	// 	}]);
 })();
